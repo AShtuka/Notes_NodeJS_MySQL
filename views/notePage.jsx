2da5528a10aa0a3a289b78ee7de23c58de3894bc
Oleksandr Shtuka <ashtuka@gmail.com>
2020-02-13 14:25:49 +0200

    work on note page

--- views/notePage.jsx
+++ views/notePage.jsx
@@ -1,12 +1,28 @@
 const React = require('react');
 const MainLayout = require('./layouts/main');
 const Navbar = require('./components/notePageNav');
+const Footer = require('./components/NoteModal/Footer');
+const Title = require('./components/NoteModal/Title');
+const Subtask = require('./components/NoteModal/Subtask');
+const ManageSubtask = require('./components/NoteModal/ManageSubtask');
 
 function NotePage(props) {
     return (
         <MainLayout {...props}>
             <Navbar />
-            <h1>Hello World</h1>
+            <div className='note-page-container grey-text text-lighten-5'>
+                <h1>Hello</h1>
+                <div className='flex-container'>
+                    <a id='addList' data-name='addList' className='footer-item' href="#">
+                        <i data-name='addList' className="material-icons position">list</i>
+                        <span data-name='addList' className='text'> Add list</span>
+                    </a>
+                    <a id='addList' data-name='addList' className='footer-item' href="#">
+                        <i data-name='addList' className="material-icons position">list</i>
+                        <span data-name='addList' className='text'> Add list</span>
+                    </a>
+                </div>
+            </div>
         </MainLayout>
     );
 }
